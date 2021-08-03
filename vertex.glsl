da19686b1c50e410abe26ffc2725356042cdb1d2
Dima Kogan <dima@secretsauce.net>
2021-08-03 15:13:09 -0700

    partial bug fix: image buffers are not upside down
    I now flip the opengl texture around to compensate for opengl treating textures
    upside down. But this means that loading images from files makes them upside
    down. Is freeimage loading images upside-down?

--- vertex.glsl
+++ vertex.glsl
@@ -12,7 +12,9 @@ void main(void)
     // vertex is in [0,1]
     // tex_xy_geometry is in [0,1]
     // gl_Position is in [-1,1]
-    tex_xy_geometry = vec2(vertex.x, vertex.y);
+
+    // I flip the image upside down since OpenGL stores it that way
+    tex_xy_geometry = 1.0 - vertex;
 
     gl_Position = vec4( (vertex - center01) / visible_width01 * 2.,
                         0, 1 );
