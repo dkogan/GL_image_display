71674c7b2e4a125c6eee2fb62d8dfd2d38c1e441
Dima Kogan <dima@secretsauce.net>
2026-08-24 21:09:25 -0700

    new mrbuild

--- choose_mrbuild.mk
+++ choose_mrbuild.mk
@@ -8,8 +8,8 @@ else ifneq (,$(wildcard /usr/include/mrbuild/Makefile.common.header))
   MRBUILD_MK=/usr/include/mrbuild
   MRBUILD_BIN=/usr/bin
 else
-  V      := 1.13
-  SHA512 := 7a1422026cdbe12cea6882c3b76087dcc4c1d258369ec2abb941a779a539253a37850563f801049d570e8ad342722030fd918114388b5155a89644491a221f16
+  V      := 1.20
+  SHA512 := 420919576bace8233a5c9833d46b52daf5e1481b2d2b531c4212ceb83c81c7131bd820e0f5463b89c7172847eafdfe724ec354d6659096c6c247a28a046f1850
   URL   := https://github.com/dkogan/mrbuild/archive/refs/tags/v$V.tar.gz
   TARGZ := mrbuild-$V.tar.gz
 
